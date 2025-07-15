afee083a77882fb947ad10c7b6f0be13c151eb89
ChipMyRide <damonakers08@gmail.com>
2025-07-15 22:23:55 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Stage1InfoPage from './pages/Stage1InfoPage';
 
-function App() {
+export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/tuning/stage1" element={<Stage1InfoPage />} />
-        {/* Weitere Seiten können hier ergänzt werden */}
+        {/* Weitere Seiten folgen später */}
       </Routes>
     </Router>
   );
 }
-
-export default App;
