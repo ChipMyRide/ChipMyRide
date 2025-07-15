4bacf69b82d19e2f87cf374c95976e975d007857
ChipMyRide <damonakers08@gmail.com>
2025-07-15 17:21:21 +0200

    Create Home.tsx

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import '../index.css';
+
+export default function Home() {
+  return (
+    <div className="text-white">
+      {/* Dein vollständiges Layout aus ChipMyRidePage.tsx einfügen */}
+      <h1 className="text-6xl font-bold text-center mt-20">Willkommen bei Chip My Ride</h1>
+      {/* … */}
+    </div>
+  );
+}
