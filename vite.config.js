cc6265e3067ee0cb9560c2b8995100fa9dec9f7d
ChipMyRide <damonakers08@gmail.com>
2025-07-16 00:03:37 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // wichtig für Vercel SPA fallback
-  resolve: {
-    conditions: ['development', 'browser'],
-  },
-});
+  base: '/',
+})
