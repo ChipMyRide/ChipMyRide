41e39736b389c88abdf632664be7de62b8ecd0b3
ChipMyRide <damonakers08@gmail.com>
2025-07-15 23:32:00 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Stage1InfoPage from './pages/Stage1InfoPage';
+import Stage2InfoPage from './pages/Stage2InfoPage';
 
 export default function App() {
   return (
@@ -9,7 +10,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/tuning/stage1" element={<Stage1InfoPage />} />
-        {/* Weitere Seiten folgen später */}
+        <Route path="/tuning/stage2" element={<Stage2InfoPage />} />
       </Routes>
     </Router>
   );
