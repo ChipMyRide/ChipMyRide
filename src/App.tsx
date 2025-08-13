80e2d5d2232743ceb4e07e967aa2ee1e0389f62f
ChipMyRide <damonakers08@gmail.com>
2025-08-14 01:04:02 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import HomePage from './pages/Home';
-import Stage1InfoPage from './pages/tuningstage1';
-import Stage2InfoPage from './pages/tuningstage2';
+import stage1InfoPage from './pages/stage1InfoPage';
+import stage2InfoPage from './pages/stage2InfoPage';
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/tuning/stage1" element={<Stage1InfoPage />} />
-        <Route path="/tuning/stage2" element={<Stage2InfoPage />} />
+        <Route path="/tuning/stage1" element={<stage1InfoPage />} />
+        <Route path="/tuning/stage2" element={<stage2InfoPage />} />
       </Routes>
     </Router>
   );
