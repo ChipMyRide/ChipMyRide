293a2d0797eedf0709276dcf87826bf4093f4019
ChipMyRide <damonakers08@gmail.com>
2025-08-14 01:51:19 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import HomePage from './pages/Home';
 import Stage1InfoPage from './pages/stage1InfoPage';
