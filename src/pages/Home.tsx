d88a97188ad3ca406f377daabc4e76e62d04ba6a
ChipMyRide <damonakers08@gmail.com>
2025-08-14 01:32:05 +0200

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,22 +10,22 @@ function Home() {
         {[
           { name: "Stage 1", price: "550 €", link: "/stage1-info-page" },
           { name: "Stage 2", price: "750 €", link: "/stage2-info-page" },
-          { name: "Stage 3", price: "950 €", link: "/tuning/stage3" },
-          { name: "Eco Tuning", price: "450 €", link: "/tuning/eco" },
-          { name: "Pops & Bangs", price: "450 €", link: "/tuning/pops" },
-          { name: "DSG Knallen", price: "150 €", link: "/tuning/dsg-knallen" },
-          { name: "Launch Control", price: "150 €", link: "/tuning/launchcontrol" },
-          { name: "Begrenzer (Vmax / RPM)", price: "150 €", link: "/tuning/begrenzer" },
-          { name: "DPF / AGR / OPF Abschaltung", price: "350 €", link: "/tuning/abschaltung" },
-          { name: "Getriebeoptimierung", price: "350 €", link: "/tuning/getriebe" },
-          { name: "VCDS Kodierung", price: "50 €", link: "/tuning/vcds" },
+          { name: "Stage 3", price: "950 €", link: "/stage3-info-page" },
+          { name: "Eco Tuning", price: "450 €", link: "/eco-info-page" },
+          { name: "Pops & Bangs", price: "450 €", link: "/pops-info-page" },
+          { name: "DSG Knallen", price: "150 €", link: "/dsg-info-page" },
+          { name: "Launch Control", price: "150 €", link: "/launchcontrol-info-page" },
+          { name: "Begrenzer (Vmax / RPM)", price: "150 €", link: "/begrenzer-info-page" },
+          { name: "DPF / AGR / OPF Abschaltung", price: "350 €", link: "/abschaltung-info-page" },
+          { name: "Getriebeoptimierung", price: "350 €", link: "/getriebe-info-page" },
+          { name: "VCDS Kodierung", price: "50 €", link: "/vcds-info-page" },
         ].map((item, index) => {
           const centerIndex = 10;
           return (
             <div
               key={index}
               className={`flex items-center justify-between bg-cyan-600 text-white rounded-2xl p-4 hover:bg-cyan-500 transition duration-300 ${
-                index === centerIndex ? 'md:col-span-2 justify-center' : ''
+                index === centerIndex ? "md:col-span-2 justify-center" : ""
               }`}
             >
               <span className="font-bold text-left w-1/3">{item.name}</span>
