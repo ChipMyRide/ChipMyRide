8bdbd37461261e9b405a680b6285395c00ab6b2c
ChipMyRide <damonakers08@gmail.com>
2025-08-14 10:22:47 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/Home';
 import Stage1InfoPage from './pages/stage1InfoPage';
 import Stage2InfoPage from './pages/stage2InfoPage';
-import NotFound from './pages/NotFound';
 
 export default function App() {
   return (
