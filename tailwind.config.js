1d959fca119085985adff095936d2cddb817146c
ChipMyRide <damonakers08@gmail.com>
2025-08-14 11:26:16 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}" // falls vorhanden
+  ],
+  theme: { extend: {} },
   plugins: [],
 }
